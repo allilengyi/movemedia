409c1b1de9b07a5b5f758d77bcb1cca644718604
mastodon <mastodon>
2019-05-30 12:05:25 +0200

    added mastodon

---
+++ mastodon/README.md
@@ -0,0 +1,11 @@
+# Start:
+
+DEBUG=myapp:* npm start
+
+# Send toot
+
+1) Open http://localhost:3000/mastodon/ in Browser
+2) Enter title and content
+3) Click 'ok'
+
+Then the toot appears on https://mastodon.technology/@movemedia
---
+++ mastodon/routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var bodyParser = require('body-parser')
+router.use( bodyParser.json() );
+
+/* mastodon */
+
+const fs = require('fs');
+var http = require('http');
+var Masto = require('mastodon')
+
+var M = new Masto({
+  access_token: 'c569d099456fb3cf55ffd11b5f4ef6d56bd8c439c630e3c4f98551db7620e5ea',
+  timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+  api_url: 'https://mastodon.technology/api/v1/', // optional, defaults to https://mastodon.social/api/v1/
+})
+
+/* > mastodon */
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/name', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../name.html'));
+});
+
+router.post('/name', function (req, res) {
+  console.log(req.body);
+  var name = req.body.name;
+  res.render('index', { title: name });
+
+});
+
+router.get('/mastodon', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../mastodon.html'));
+});
+
+router.post('/mastodon', function (req, res) {
+  console.log(req.body);
+  var title = req.body.title;
+  var content = req.body.content;
+
+  M.post('statuses', { status: title + ": " + content})
+
+  res.render('mastodon', { title: title, content: content});
+
+});
+
+module.exports = router;
